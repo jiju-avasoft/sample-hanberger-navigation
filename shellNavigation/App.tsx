f704f5b66d8193efaa9321d0914ad6cf55178ce8
Jiju  Selvakumar <Jiju.S@avasoft.com>
2022-12-29 19:50:37 +0530

    Initial commit

---
+++ shellNavigation/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const App = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#ffffff',
+      }}>
+      <Text style={{color: '#000000', fontSize: 32}}>Helooo</Text>
+    </View>
+  );
+};
+
+export default App;
